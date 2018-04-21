074dcdd50ec236fed8b231d44e363c4eb0e2782c
James Milner <james.milner@virgin.net>
2018-04-21 01:48:23 +0100

    Added layerdata route

--- lib/app.rb
+++ lib/app.rb
@@ -1,20 +1,43 @@
 require "sinatra/base"
 require "file_processor"
 require "json"
+require "httparty"
+require "tempfile"
+
 
 class App < Sinatra::Base
-  
+
   set :views,  File.expand_path('../../views', __FILE__)
   set :public_folder, File.expand_path('../../views', __FILE__)
   set :data_folder, File.expand_path('../../data', __FILE__)
-  
-  
-  
+
+
+
   get '/' do
     puts settings.data_folder.inspect
-    haml :index 
+    haml :index
   end
-  
+
+  get '/layerdata/:type' do
+    query = {
+      type: params[:type],
+      bbox: params[:BBOX],
+      zoom: 11
+    }
+    base_url = 'https://webkaart.hoogspanningsnet.com/layerdata.php'
+    if (response = HTTParty.get(base_url, query: query)) && response.success?
+      json_file = Tempfile.new('hoogspan_json')
+      kml_file = Tempfile.new('hoogspan_kml')
+      File.open(json_file.path,'wb') {|f| f << response.to_s}
+      #{}%x(/Users/james/node_modules/tokml/tokml #{json_file.path} > #{kml_file.path})
+      #%x(/Users/james/node_modules/tokml/tokml #{json_file.path})
+      %x(ogr2ogr -f KML #{kml_file.path} #{json_file.path})
+      File.read(kml_file.path)
+    else
+      halt 404
+    end
+  end
+
   post '/process' do
     puts params.inspect
     filename = params["0"][:filename]
@@ -29,9 +52,9 @@ class App < Sinatra::Base
       end
     end
   end
-  
+
   get '/download/:id.?:format?' do
-    send_file File.join(settings.data_folder, "eigrid-#{params[:id]}.#{params[:format]}")    
+    send_file File.join(settings.data_folder, "eigrid-#{params[:id]}.#{params[:format]}")
   end
-  
-end  
+
+end
